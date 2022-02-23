f2c93aa11ab9a3d799147959f87c3bd4831f245e
Jaziel Perez <pjaziel723@gmail.com>
2022-02-23 01:16:41 -0600

    se agrego el archivo funciones

--- funciones.php
+++ funciones.php
@@ -1,6 +1,23 @@
 <?php
-$nombre = "camila";
 
-$apelliod = "Rojas"."\n";
 
-echo $nombre. " " . $apelliod;
+function concatenar($nombre, $apellido){
+    echo "Su nombre es $nombre y su apellido $apellido" . "\n";
+
+}
+concatenar("Jaziel","Perez");
+
+function sumarNum($numeros){
+    $suma = 0;
+for ($i=0; $i < 5 ; $i++) { 
+    $suma = $suma + $numeros[$i];
+}
+echo "La suma de los numeros es: $suma" . "\n";
+
+}
+
+$numeros = array(1,2,3,4,5);
+sumarNum($numeros)
+
+?>
+
