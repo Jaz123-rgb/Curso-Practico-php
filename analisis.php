5dacf018eb815f782a75ccacb4a47ff7de2202c3
Jaziel Perez <pjaziel723@gmail.com>
2022-02-24 02:58:40 -0600

    as

--- analisis.php
+++ analisis.php
@@ -1,14 +1,18 @@
 <?php
-
-
 $palabras = array("sol","luna","cielo", "luz", "pendejo");
+
+
 
 for ($i=0; $i < count($palabras) ; $i++) { 
     
     if($_REQUEST["palabra".$i]==$palabras[$i]){
        echo"la palabra ingresada es correcta". "<br>";
 
+    }else{
+
+        echo"la palabra es incorrecta, la palabra correcta es:". $palabras[$i] . "<br>" ;
     }
+
 
 }
 
